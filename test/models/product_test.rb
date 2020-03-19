a30d0614d047cf4c198bdc39cad54caca459af02
NgocPH <phamhuungoc050798@gmail.com>
2020-03-19 14:09:08 +0700

    Validation ver2

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,6 +9,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -19,9 +20,9 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title:       "My Book Title",
+    product = Product.new(title: "My Book Title",
                           description: "yyy",
-                          image_url:   "zzz.jpg")
+                          image_url: "zzz.jpg")
     product.price = -1
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
@@ -37,17 +38,15 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   def new_product(image_url)
-    Product.new(title:       "My Book Title",
+    Product.new(title: "My Book Title",
                 description: "yyy",
-                price:       1,
-                image_url:   image_url)
+                price: 1,
+                image_url: image_url)
   end
 
   test "image url" do
-    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-             http://a.b.c/x/y/z/fred.gif }
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
-    
     ok.each do |name|
       assert new_product(name).valid?, "#{name} shouldn't be invalid"
     end
@@ -58,24 +57,21 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product is not valid without a unique title" do
-    product = Product.new(title:       products(:ruby).title,
+    product = Product.new(title: products(:ruby).title,
                           description: "yyy", 
-                          price:       1, 
-                          image_url:   "fred.gif")
-
+                          price: 1, 
+                          image_url:"fred.gif")
     assert product.invalid?
     assert_equal ["has already been taken"], product.errors[:title]
   end
 
   test "product is not valid without a unique title - i18n" do
-    product = Product.new(title:       products(:ruby).title,
+    product = Product.new(title: products(:ruby).title,
                           description: "yyy", 
-                          price:       1, 
-                          image_url:   "fred.gif")
-
+                          price: 1, 
+                          image_url: "fred.gif")
     assert product.invalid?
-    assert_equal [I18n.translate('errors.messages.taken')],
-                 product.errors[:title]
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
   
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,9 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # OLD: parallelize(workers: :number_of_processors)  <--
+  parallelize(workers: 1) # i.e don't run tests in parallel via forked processes
+  # OR just comment the line above out completely <--
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
