7b9080917af60acd28dc204d9a3e3529fc0d2d6a
NgocPH <phamhuungoc050798@gmail.com>
2020-03-19 14:58:02 +0700

    Catalog Display

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_035006) do
+ActiveRecord::Schema.define(version: 2020_03_19_075618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
