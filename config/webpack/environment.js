2b03bc059944feb67911c6e424ba3c16046ef2d1
NgocPH <phamhuungoc050798@gmail.com>
2020-03-20 15:17:00 +0700

    Task 11

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,26 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.loaders.get('sass').use.splice(-1, 0, {
+    loader: 'resolve-url-loader',
+    options: {
+        attempts: 1
+    }
+});
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+        $: 'jquery',
+        JQuery: 'jquery',
+        jquery: 'jquery',
+        'window.Tether': "tether",
+        Popper: ['popper.js', 'default'], // for Bootstrap 4
+    })
+)
+const aliasConfig = {
+    'jquery': 'jquery/src/jquery',
+    'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+
+};
+
+environment.config.set('resolve.alias', aliasConfig);
 module.exports = environment
