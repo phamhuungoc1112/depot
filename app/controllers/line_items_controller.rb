3268316603ef285e89567bff7a36ce278a1b574d
NgocPH <phamhuungoc050798@gmail.com>
2020-03-20 11:39:39 +0700

    Ajax F4

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -39,8 +39,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
+        format.js   { @current_item = @line_item }
         format.json { render :show,
           status: :created, location: @line_item }
       else
@@ -81,9 +81,10 @@ class LineItemsController < ApplicationController
       @line_item = LineItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, only allow the white
+    # list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
   #...
 end
